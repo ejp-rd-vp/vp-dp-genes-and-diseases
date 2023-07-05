d8cef042bc383aa547488a1c455ece41d1723814
BARAKAT <ammrbr@hotmail.com>
2023-07-05 14:55:26 +0200

    #1 - Add Transactional Annotation for Improved Data Integrity and Consistency
    - Add Transactional Annotation for Improved Data Integrity and Consistency

--- src/main/java/org/ejprarediseases/vpsearch/services/GenesService.java
+++ src/main/java/org/ejprarediseases/vpsearch/services/GenesService.java
@@ -1,5 +1,6 @@
 package org.ejprarediseases.vpsearch.services;
 
+import jakarta.transaction.Transactional;
 import org.ejprarediseases.vpsearch.models.gene.GeneMapping;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,6 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class GenesService {
 
+    @Transactional
     public GeneMapping getGeneMapping(String hgncId) {
         WebClient webClient = WebClient.create("http://155.133.131.171:8080/GENES/gendis/find?by=hgnc&input=" + hgncId);
         String response = webClient.get().retrieve().bodyToMono(String.class).block();
